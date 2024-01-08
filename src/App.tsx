fd8738f0615cdb12254663562d11a479c8c23a29
IvkovicDanica <ivkovicdanica555@gmail.com>
2024-01-08 23:14:37 +0100

    Dodato fetchovanje usera i loader komponenta

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { User } from './model';
+import { getUser } from './service/services';
+import Loader from './components/Loader';
 
 function App() {
+  const [user, setUser] = useState<User | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (!loading) {
+      return;
+    }
+    getUser()
+      .then(setUser)
+      .catch(err => { })
+      .finally(() => setLoading(false))
+  }, [loading])
+
+  if (loading) {
+    return (
+      <div className='main'>
+        <Loader />
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
